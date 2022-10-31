365ea18ad59780edacded759bd8601fcc4748254
Oksana Lobach <oksanalobach247@gmail.com>
2022-10-31 10:23:26 +0100

    make HW

--- FrontEnd/HomeWork/HW_25_10/script.js
+++ FrontEnd/HomeWork/HW_25_10/script.js
@@ -2,7 +2,7 @@ console.log("First level: 1. Фильтрация по диапазону Нап
 
 
 let arr = [5, 3, 8, 1];
-
+/*
 function filterRange(arr, a, b){
 
     let filteredArr = [];
@@ -15,7 +15,7 @@ filteredArr.push(element);
 }
 console.log(filterRange(arr,1,4));
 console.log(arr);
-
+*/
 //Пыталась сделать через фильтр, но не знаю как вписать границы диапазона
 /*let result =arr.filter(function(item){
 return (item>=1 && item<=4)
@@ -24,13 +24,15 @@ console.log(result);*/
 
 
 console.log("Напишите функцию filterRangeInPlace(arr, a, b), которая принимает массив arr и удаляет из него все значения кроме тех, которые находятся между a и b. То есть, проверка имеет вид a ≤ arr[i] ≤ b.Функция должна изменять принимаемый массив и ничего не возвращать.")
-/*
+
 function filterRange(arr, a, b){
  for (let i = 0; i < arr.length; i++) {
         const element = arr[i];
         if(element<a|| element>b)
 arr.splice(i,1)
+i--;
     }
     console.log(arr)
 }
-console.log(filterRange(arr,1,4))*/
+console.log(filterRange(arr,1,4))
+
